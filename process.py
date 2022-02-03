c3610b8b808e173f79f23bc3c6731693f58b1623
Antus8 <antus.longo@gmail.com>
2022-02-03 10:27:59 +0100

    Add Logistic Regression

--- process.py
+++ process.py
@@ -13,12 +13,12 @@ from pyspark import SparkContext,SparkConf
 import torchvision.transforms as transforms
 from pyspark.ml.linalg import Vectors, VectorUDT
 from pyspark.sql.functions import udf
-from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.feature import IndexToString, StringIndexer, VectorIndexer
 from pyspark.sql.types import StringType, ArrayType,StructType,StructField, FloatType
+from pyspark.ml.classification import RandomForestClassifier, GBTClassifier, LogisticRegression
 
-# spark = SparkSession.builder.appName('Artifact Classification').getOrCreate()
+spark = SparkSession.builder.appName('Artifact Classification').getOrCreate()
 outfile = '/home/antonello/Scrivania/Artifact-Classification-Apache-Spark/dataset.npz'
 
 npzfile = np.load(outfile)
@@ -30,7 +30,7 @@ for key in npzfile.files:
     	dataset.append({"label" : key, "features" : feature_vector.tolist()})
 
 df = spark.createDataFrame(dataset)
-df.printSchema()
+# df.printSchema()
 
 list_to_vector_udf = udf(lambda l: Vectors.dense(l), VectorUDT())
 df = df.select(
@@ -38,7 +38,11 @@ df = df.select(
     list_to_vector_udf(df["features"]).alias("vector_features")
 )
 
-print("ML ALGORITHM RANDOM FOREST CLASSIFIER")
+'''
+###############################
+RANDOM FOREST CLASSIFIER
+###############################
+'''
 
 '''
 Solo pandas riesce a leggere bene i numpy arrays ma poi ci sono problemi con il modello di ML
@@ -68,13 +72,44 @@ model = pipeline.fit(trainingData)
 
 predictions = model.transform(testData)
 
-predictions.select("predictedLabel", "label", "vector_features").show(5)
+predictions.show(50)
 
 
 evaluator = MulticlassClassificationEvaluator(
     labelCol="label_indexed", predictionCol="prediction", metricName="accuracy")
-accuracy = evaluator.evaluate(predictions)
-print("Test Error = %g" % (1.0 - accuracy))
+rf_accuracy = evaluator.evaluate(predictions)
+
+'''
+###############################
+MULTINOMIAL LOGISTIC REGRESSION
+###############################
+'''
+
+df = df.select(
+    df["label"].alias("string_labels"), 
+    df["vector_features"].alias("features")
+)
+
+labelIndexer = StringIndexer(inputCol="string_labels", outputCol="label").fit(df)
+(trainingData, testData) = df.randomSplit([0.7, 0.3])
+
+lr = LogisticRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
+
+labelConverter = IndexToString(inputCol="prediction", outputCol="predictedLabel",
+                               labels=labelIndexer.labels)
+pipeline = Pipeline(stages=[labelIndexer, lr, labelConverter])
+model = pipeline.fit(trainingData)
+predictions = model.transform(testData)
+
+predictions.show(50)
+
+evaluator = MulticlassClassificationEvaluator(
+    labelCol="label", predictionCol="prediction", metricName="accuracy")
+lr_accuracy = evaluator.evaluate(predictions)
+
+print("Random forest accuracy is: " + str(rf_accuracy))
+print("Logistic regression accuracy is: " + str(lr_accuracy))
+
 
 spark.stop()
 
